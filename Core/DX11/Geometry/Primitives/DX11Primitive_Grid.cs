532393c89e9b856651dbf3655b5b31e88ab327c2
mrvux <julien@mrvux.com>
2013-02-27 00:15:53 +0100

    -Fixed winding on grid

--- Core/DX11/Geometry/Primitives/DX11Primitive_Grid.cs
+++ Core/DX11/Geometry/Primitives/DX11Primitive_Grid.cs
@@ -71,11 +71,12 @@ namespace FeralTic.DX11.Geometry
                     int col = i * (resX - 1);
 
                     indlist.Add(0 + rowlow + i);
-                    indlist.Add(1 + rowlow + i);
                     indlist.Add(0 + rowup + i);
                     indlist.Add(1 + rowlow + i);
+
+                    indlist.Add(1 + rowlow + i);
+                    indlist.Add(0 + rowup + i);
                     indlist.Add(1 + rowup + i);
-                    indlist.Add(0 + rowup + i);
                 }
             }
 
