e16e1933452878e924413584f8b60a8a244fae6d
mrvux <julien@mrvux.com>
2013-05-29 13:59:08 +0200

    Descriptor for sphere

--- Core/DX11/Geometry/Primitives/DX11Primitive_Sphere.cs
+++ Core/DX11/Geometry/Primitives/DX11Primitive_Sphere.cs
@@ -15,8 +15,14 @@ namespace FeralTic.DX11.Geometry
 {
     public partial class DX11PrimitivesManager
     {
-        public DX11IndexedGeometry Sphere(float radius, int resX, int resY, float cx, float cy)
+        public DX11IndexedGeometry Sphere(Sphere settings)
         {
+            float radius = settings.Radius;
+            int resX = settings.ResX;
+            int resY = settings.ResY;
+            float cx = settings.CyclesX;
+            float cy = settings.CyclesY;
+
             List<Pos4Norm3Tex2Vertex> vertices = new List<Pos4Norm3Tex2Vertex>();
             List<int> indices = new List<int>();
 
@@ -79,6 +85,8 @@ namespace FeralTic.DX11.Geometry
             DX11IndexedGeometry geom = DX11IndexedGeometry.CreateFrom<Pos4Norm3Tex2Vertex>(context, vertices.ToArray(), indices.ToArray(), Pos4Norm3Tex2Vertex.Layout);               
             geom.HasBoundingBox = true;
             geom.BoundingBox = new BoundingBox(new Vector3(-radius), new Vector3(radius));
+            geom.Tag = settings;
+            geom.PrimitiveType = "Sphere";
 
             return geom;
         }
