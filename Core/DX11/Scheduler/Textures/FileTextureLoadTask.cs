18b471325b115e8d3492b26e133bf45f77eb4c93
Julien Vulliet <julien@mrvux.com>
2013-12-04 15:10:25 +0100

    -File texture task has mips options

--- Core/DX11/Scheduler/Textures/FileTextureLoadTask.cs
+++ Core/DX11/Scheduler/Textures/FileTextureLoadTask.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FeralTic.DX11.Resources;
+using SlimDX.Direct3D11;
 
 namespace FeralTic.DX11
 {
@@ -24,14 +25,23 @@ namespace FeralTic.DX11
 
     public class FileTexture2dLoadTask : FileTextureLoadTask<DX11Texture2D>
     {
-        public FileTexture2dLoadTask(DX11RenderContext context, string path)
+        private bool domips;
+
+        public FileTexture2dLoadTask(DX11RenderContext context, string path, bool domips)
             : base(context, path)
         {
+            this.domips = domips;
         }
 
         protected override void DoProcess()
         {
-            this.Resource = DX11Texture2D.FromFile(this.Context, path);
+            ImageLoadInformation info = ImageLoadInformation.FromDefaults();
+            if (!this.domips)
+            {
+                info.MipLevels = 1;
+            }
+
+            this.Resource = DX11Texture2D.FromFile(this.Context, path,info);
         }
     }
 
