11bcb2a3a375ec1b87c57ce2f96886407782d32f
Julien Vulliet <julien@mrvux.com>
2015-02-13 10:28:20 +0100

    Add index only drawer

---
+++ Core/DX11/Resources/Geometry/Drawers/DX11IndexOnlyDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX.Direct3D11;
+
+namespace FeralTic.DX11.Resources
+{
+    public class DX11IndexOnlyDrawer : IDX11GeometryDrawer<DX11IndexedGeometry>
+    {
+        protected DX11IndexedGeometry geom;
+
+        public void Assign(DX11IndexedGeometry geometry)
+        {
+            this.geom = geometry;
+        }
+
+        public void PrepareInputAssembler(DeviceContext ctx, InputLayout layout)
+        {
+            VertexBufferBinding vb = new VertexBufferBinding();
+            ctx.InputAssembler.SetVertexBuffers(0, vb);
+            geom.IndexBuffer.Bind();
+        }
+
+        public virtual void Draw(DeviceContext ctx)
+        {
+            ctx.DrawIndexed(this.geom.IndexBuffer.IndicesCount, 0, 0);
+        }
+    }
+}
