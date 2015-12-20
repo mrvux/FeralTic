f9f5f5c2dbd4982ad4226568feb6712a7dd2ec24
Julien Vulliet <julien@mrvux.com>
2015-12-20 20:39:34 +0300

    Some double defaults

--- Core/Utils/MagicNumberUtils.cs
+++ Core/Utils/MagicNumberUtils.cs
@@ -11,5 +11,15 @@ namespace FeralTic.Utils
     public static class MagicNumberUtils
     {
         public const int FeatureLevel11_1 = 45312;
+
+        public static double[] ToDoubleArray(this SlimDX.Color4 color)
+        {
+            return new double[] { color.Red, color.Green, color.Blue, color.Alpha };
+        }
+
+        public static double[] WhiteDefault()
+        {
+            return new double[] { 1.0,1.0,1.0,1.0 };
+        }
     }
 }
