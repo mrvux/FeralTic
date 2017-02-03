325b04e0177bcf933beef205bdefd941d6731575
Julien Vulliet <julien@mrvux.com>
2017-02-03 17:48:16 +0100

    Add more vertex layouts
    Add a preset vertex layout

---
+++ Core/DX11/Geometry/VertexLayouts/Pos4Norm3Vertex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+using SlimDX;
+using SlimDX.Direct3D11;
+
+namespace FeralTic.DX11.Geometry
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Pos4Norm3Vertex
+    {
+        public Vector4 Position;
+        public Vector3 Normals;
+
+        private static InputElement[] layout;
+
+        public static InputElement[] Layout
+        {
+            get
+            {
+                if (layout == null)
+                {
+                    layout = new InputElement[]
+                    {
+                        new InputElement("POSITION",0,SlimDX.DXGI.Format.R32G32B32A32_Float,0, 0),
+                        new InputElement("NORMAL",0,SlimDX.DXGI.Format.R32G32B32_Float,16,0),
+                    };
+                }
+                return layout;
+            }
+        }
+
+        public static int VertexSize
+        {
+            get { return Marshal.SizeOf(typeof(Pos4Norm3Vertex)); }
+        }
+    }
+}
---
+++ Core/DX11/Utils/VertexLayoutsHelpers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeralTic.DX11.Geometry;
+using SlimDX.Direct3D11;
+
+namespace FeralTic.DX11.Utils
+{
+    public static class VertexLayoutsHelpers
+    {
+        public const string VertexLayoutsEnumName = "DX11_Vertexlayouts_Presets";
+        private static string[] enumEntries;
+
+        private static readonly IReadOnlyDictionary<string, InputElement[]> elements;
+
+        public static IReadOnlyList<string> Entries
+        {
+            get { return enumEntries; }
+        }
+
+        public static IReadOnlyDictionary<string, InputElement[]> Elements
+        {
+            get { return elements; }
+        }
+
+        static VertexLayoutsHelpers()
+        {
+            Dictionary<string, InputElement[]> data = new Dictionary<string, InputElement[]>();
+
+            data.Add("Pos3", new InputElement[] { new InputElement("POSITION", 0, SlimDX.DXGI.Format.R32G32B32_Float, 0, 0) });
+            data.Add("Pos4", new InputElement[] { new InputElement("POSITION", 0, SlimDX.DXGI.Format.R32G32B32A32_Float, 0, 0) });
+
+            data.Add("Pos3Norm3", Pos3Norm3Vertex.Layout);
+            data.Add("Pos4Norm3", Pos4Norm3Vertex.Layout);
+
+            data.Add("Pos3Norm3Tex2", Pos3Norm3Tex2Vertex.Layout);
+            data.Add("Pos4Norm3Tex2", Pos4Norm3Tex2Vertex.Layout);
+
+            data.Add("Pos2Tex2", Pos2Tex2Vertex.Layout);
+            data.Add("Pos3Tex2", Pos3Tex2Vertex.Layout);
+            data.Add("Pos4Tex2", Pos4Tex2Vertex.Layout);
+
+            elements = data;
+            enumEntries = data.Keys.ToArray();
+        }
+
+
+    }
+}
