9f21f43216fcc42a98c43b58a15feecc92cd56a4
Julien Vulliet <julien@mrvux.com>
2017-02-05 22:58:17 +0100

    Add dxgi utils

---
+++ Core/DXGI/DisplayModeUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SlimDX.DXGI;
+
+namespace FeralTic.DXGI
+{
+    public class DisplayModeUtils
+    {
+        public static ModeDescription GetClosestDisplayMode(Output o, Device device, SlimDX.Rational rational)
+        {
+            var pt = new System.Drawing.Point(o.Description.DesktopBounds.Left + 1, o.Description.DesktopBounds.Top + 1);
+
+            Screen scr = Screen.FromPoint(pt);
+
+            ModeDescription md = new ModeDescription(scr.Bounds.Width, scr.Bounds.Height, rational, Format.R8G8B8A8_UNorm);
+            ModeDescription closest;
+            o.GetClosestMatchingMode(device, md, out closest);
+            return closest;
+        }
+    }
+}
